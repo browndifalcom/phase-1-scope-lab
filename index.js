134ff9fedd2b5183c7561a38425d2eb6f69aa241
Michael <browndifalcom@gmail.com>
2022-01-08 11:59:50 -0500

    commit tree fef406717e83bf193505504e0166d8b02051aec4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'michael';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Matthew';
+    return leastFavoriteCustomer;
+}
